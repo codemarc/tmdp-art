28ea51964ab2296ca22acc323fc229a1f4b49468
marc j. greenberg <marc@buildinglink.com>
2024-05-22 14:46:12 -0400

    homepage update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
-import img1 from './codemarcart.png';
+import img1 from "./codemarcart.png";
 
 function App() {
-  const topStyle = {
-    height: '100%',
-    width: '100%',
-    opacity: '1.0',
-  }
-
   return (
-    <div className="App">
-
-      <div style={{
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-      }}>
-        <a href="/" target="_blank" rel="noreferrer">
-        <img src={img1} alt="CodeMarcArt" style={topStyle} />
-        </a>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <a href="/milagro/docs/#/" target="_blank" rel="noreferrer">
-            The Miracle Development Process
-          </a>
+    <div className="App" style={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '75vh', // assuming you want to center it in the whole viewport height
+    }}>
+      <div style={{ marginBottom: '10px' }}>
+      <a
+                href="https://codemarc.net/miracle/doc/#/"
+                target="_blank"
+                rel="noreferrer"
+                style={{ textAlign:"center", textShadow: "1px 1px darkgreen" }}
+              >
+                CodeMarcArt is a Product of The Miracle Development Process.
+                To learn more click here
+              </a>        
+        
         </div>
+      <div>
+        <a href="/" target="_blank" rel="noreferrer">
+          <img src={img1} style={{border:"1px solid grey"}}alt="CodeMarcArt" />
+          </a>
       </div>
     </div>
   )
